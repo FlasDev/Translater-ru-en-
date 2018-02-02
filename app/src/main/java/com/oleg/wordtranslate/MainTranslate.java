8abc9ab069822098c493ff3c47f2b3669b8635ac
Oleg Chausov <olegchausov98@gmail.com>
2018-02-02 16:23:49 +0200

    Second Commit

---
+++ app/src/main/java/com/oleg/wordtranslate/MainTranslate.java
@@ -0,0 +1,26 @@
+package com.oleg.wordtranslate;
+
+/**
+ * Created by oleg on 02.02.2018.
+ */
+
+public class MainTranslate {
+    private String mWord;
+    private String mTranslate;
+
+    public String getWord() {
+        return mWord;
+    }
+
+    public void setWord(String word) {
+        mWord = word;
+    }
+
+    public String getTranslate() {
+        return mTranslate;
+    }
+
+    public void setTranslate(String translate) {
+        mTranslate = translate;
+    }
+}
--- app/src/main/java/com/oleg/wordtranslate/TranslatorFragment.java
+++ app/src/main/java/com/oleg/wordtranslate/TranslatorFragment.java
@@ -3,17 +3,26 @@ package com.oleg.wordtranslate;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by oleg on 02.02.2018.
  */
 
 public class TranslatorFragment extends Fragment {
-
+    @BindView(R.id.fragment_translator_input_word) TextInputEditText mWordField;
+    @BindView(R.id.fragment_translator_input_translate) TextInputEditText mTranslateField;
+    @BindView(R.id.fragment_translator_button_translate) Button mTranslateButton;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +34,16 @@ public class TranslatorFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_translator, container, false);
+        ButterKnife.bind(this,view);
         return view;
     }
 
     public static TranslatorFragment newInstance(){
         return new TranslatorFragment();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.translator_fragment,menu);
     }
 }
