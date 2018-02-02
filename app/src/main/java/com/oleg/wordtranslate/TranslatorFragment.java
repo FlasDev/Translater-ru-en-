03f2b905e42375bbb33850b0a22c1a2842dd370a
Oleg Chausov <olegchausov98@gmail.com>
2018-02-02 14:58:09 +0200

    First Commit

---
+++ app/src/main/java/com/oleg/wordtranslate/TranslatorFragment.java
@@ -0,0 +1,34 @@
+package com.oleg.wordtranslate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by oleg on 02.02.2018.
+ */
+
+public class TranslatorFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_translator, container, false);
+        return view;
+    }
+
+    public static TranslatorFragment newInstance(){
+        return new TranslatorFragment();
+    }
+}
